78b808e9305bfcabaa861077668ee0fc6ebeb47b
Dylan Lewis <d.lewis.1@research.gla.ac.uk>
2024-03-18 10:04:25 +0000

    WIP

--- R/mod_pt_AreaMap3.R
+++ R/mod_pt_AreaMap3.R
@@ -81,7 +81,7 @@ mod_pt_AreaMap3_server <- function(id, r, varbl, categ){
         #scaledValues <- (values$scaled-min(values$scaled))/(max(values$scaled)-min(values$scaled))
         scaledValues <- pmax(pmin((values$scaled/10)+0.5, 1), 0)
         colr <- as.data.frame(colorRamp(c("white", "#005CBA"))(scaledValues)) %>%
-          purrr::pmap_chr(~rgb(..1,..2,..3, maxColorValue = 255))
+          purrr::pmap_chr(~if_else(is.na(..1), rgb(0.5,0.5,0.5), rgb(..1,..2,..3, maxColorValue = 255)))
 
         if(codebook$mean[codebook$obs==varbl()]==T){
           labl <- paste0(mapDat()$ward_name, ": ", values$labelled)
