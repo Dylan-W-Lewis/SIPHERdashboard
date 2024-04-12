78d93d3257c22d4d6ff7b82b249266943638107a
Dylan Lewis <d.lewis.1@research.gla.ac.uk>
2024-04-12 16:27:58 +0100

    update data to new versions

--- R/utils_server.R
+++ R/utils_server.R
@@ -7,21 +7,41 @@
 #' @noRd
 #'
 #' @examples
-#' get_cats("cares for sick/disabled/elderly in household")
+#' get_cats("racel_dv")
 get_cats <- function(var) {
-  if(!var %in% codebook$obs) {
-    warning(paste0("variable '", var, "' not found in codebook"))
+  if(!var %in% reference$obs) {
+    warning(paste0("variable '", var, "' not found"))
     cats <- NULL
   }
   else {
-    cats <- dplyr::pull(codebook$cat[codebook$obs==var][[1]])
+    cats <- dplyr::pull(reference$cats[codebook$obs==var][[1]])
   }
   return(cats)
 }
 
+#' var_names
+#'
+#' @description combine variable names into a nice sentence
+#'
+#' @param vars vector of variable names
+#'
+#' @noRd
 var_names <- function(vars){
   quoted <- paste0("'", vars, "'")
   commas <- paste0(quoted[1:length(quoted)-1], collapse = ", ")
   full <- paste0(commas, ", and ", quoted[length(quoted)])
   return(full)
+}
+
+#' translate_codes
+#'
+#' @description get human friendly names from variable/category codes
+#'
+#' @param codes character vector of codes
+#'
+#' @noRd
+translate_codes <- function(codes){
+  purrr::walk(codes, ~if(! .x %in% codebook$code){warning(paste(.x, "not found in codebook"))})
+  out <- codebook$name[match(codes, codebook$code)]
+  return(out)
 }
